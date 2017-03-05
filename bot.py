f8c2390076f425482531d436cad0de2e6402b778
Ironjanowar <ironjanowar@gmail.com>
2017-03-05 23:26:20 +0100

    Meh 3

--- bot.py
+++ bot.py
@@ -99,7 +99,7 @@ def auto_update(message):
     else:
         bot.reply_to(message, "Este comando es solo para admins y debe ser enviado por privado")
 
-@bot.message_handler(True)
+@bot.message_handler(func=lambda x: x == x)
 def reproduce(m):
     reproduceYoutube(m.text.split(" ", 1)[1], m)
     if not queue:
